433bbe4c95539aa00d0aac0c77d3eee5a0ce3c0a
Francesco Ceccon <francesco@ceccon.me>
2024-05-09 17:59:14 +0200

    enable apcupsd

--- hosts/validator01/configuration.nix
+++ hosts/validator01/configuration.nix
@@ -8,46 +8,7 @@
     inputs.self.nixosModules.monitoring
   ];
 
-  services.validator = {
-    enable = true;
-    chain = "holesky";
-    validator-env = config.age.secrets.validator-env.path;
-    web3.jwtsecret = config.age.secrets.jwtsecret.path;
-    prysm-validator.wallet-password = config.age.secrets.validator-wallet-password.path;
-    mev-boost = {
-      min-bid = "0.01";
-      relays = [
-        "https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-holesky.flashbots.net"
-        "https://0xab78bf8c781c58078c3beb5710c57940874dd96aef2835e7742c866b4c7c0406754376c2c8285a36c630346aa5c5f833@holesky.aestus.live"
-        "https://0xaa58208899c6105603b74396734a6263cc7d947f444f396a90f7b7d3e65d102aec7e5e5291b27e08d02c50a050825c2f@holesky.titanrelay.xyz"
-      ];
-    };
-    sync = {
-      checkpoint = "https://holesky.beaconstate.info/";
-      genesis = "https://holesky.beaconstate.info/";
-    };
-  };
-
-  services.monitoring = {
-    enable = true;
-    user = config.services.validator.user.name;
-    otel-env = config.age.secrets.otel-env.path;
-    services = [
-      {
-        unit = "holesky-nethermind";
-        port = config.services.validator.nethermind.metrics.port;
-      }
-      {
-        unit = "holesky-prysm";
-        port = config.services.validator.prysm.metrics.port;
-      }
-      {
-        unit = "holesky-validator";
-        port = config.services.validator.prysm-validator.metrics.port;
-      }
-    ];
-  };
-
+  # Secrets needed by other services
   age.secrets.tailscale.file = ./secrets/tailscale.age;
   age.secrets.password.file = ./secrets/password.age;
   age.secrets.jwtsecret = {
@@ -75,6 +36,52 @@
     mode = "0440";
   };
 
+  # Run a validator on the Holesky testnet
+  services.validator = {
+    enable = true;
+    chain = "holesky";
+    validator-env = config.age.secrets.validator-env.path;
+    web3.jwtsecret = config.age.secrets.jwtsecret.path;
+    prysm-validator.wallet-password = config.age.secrets.validator-wallet-password.path;
+    mev-boost = {
+      min-bid = "0.01";
+      relays = [
+        "https://0xafa4c6985aa049fb79dd37010438cfebeb0f2bd42b115b89dd678dab0670c1de38da0c4e9138c9290a398ecd9a0b3110@boost-relay-holesky.flashbots.net"
+        "https://0xab78bf8c781c58078c3beb5710c57940874dd96aef2835e7742c866b4c7c0406754376c2c8285a36c630346aa5c5f833@holesky.aestus.live"
+        "https://0xaa58208899c6105603b74396734a6263cc7d947f444f396a90f7b7d3e65d102aec7e5e5291b27e08d02c50a050825c2f@holesky.titanrelay.xyz"
+      ];
+    };
+    sync = {
+      checkpoint = "https://holesky.beaconstate.info/";
+      genesis = "https://holesky.beaconstate.info/";
+    };
+  };
+
+  # Monitor the validator services
+  services.monitoring = {
+    enable = true;
+    user = config.services.validator.user.name;
+    otel-env = config.age.secrets.otel-env.path;
+    services = [
+      {
+        unit = "holesky-nethermind";
+        port = config.services.validator.nethermind.metrics.port;
+      }
+      {
+        unit = "holesky-prysm";
+        port = config.services.validator.prysm.metrics.port;
+      }
+      {
+        unit = "holesky-validator";
+        port = config.services.validator.prysm-validator.metrics.port;
+      }
+    ];
+  };
+
+  # Configuration specific to this machine
+  services.apcupsd = {
+    enable = true;
+  };
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
