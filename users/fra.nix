56c04721df83d077912249652c016117c8f377d1
Francesco Ceccon <francesco@ceccon.me>
2024-05-09 16:55:26 +0200

    add laptop public key

--- users/fra.nix
+++ users/fra.nix
@@ -4,7 +4,7 @@ let
 in
 {
   users.users.fra = {
-    openssh.authorizedKeys.keyFiles = [ ./keys/fra-danix ];
+    openssh.authorizedKeys.keyFiles = [ ./keys/fra-danix ./keys/fra-t14s ];
     hashedPasswordFile = config.age.secrets.password.path;
 
     useDefaultShell = true;
