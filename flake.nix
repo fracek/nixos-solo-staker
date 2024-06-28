aaa7ba375c0752f834984fa60f0a3a4d6c5e60ed
Francesco Ceccon <francesco@ceccon.me>
2024-06-28 13:01:16 +0200

    update flakes

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A bootable NixOS system for solo staking.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     agenix.url = "github:ryantm/agenix";
     ethereum.url = "github:nix-community/ethereum.nix";
